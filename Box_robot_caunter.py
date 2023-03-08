31d1fd487a390c588a632117287d71447c71fc21
Andrew Sulym <a.sulymm@gmail.com>
2023-03-08 23:59:32 +0000

    add chek len of array

--- Box_robot_caunter.py
+++ Box_robot_caunter.py
@@ -1,4 +1,17 @@
+arry = [1,2,3,4,5]
+arry_fals = [1,2,3,0,1]
+arry_pust = []
+arry_one = [42]
 
 def is_sorted(box_numbers: list) -> bool:
     # write your code here
-1111111111111
+    for i in box_numbers:
+        if len(box_numbers) == 0 or len(box_numbers) == 1:
+            return True
+      
+    return True
+        
+        
+
+w = is_sorted(arry)
+print(w)
