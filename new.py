a490258b357b980ff5f36e44596aaf6d5c1810fe
Andrew Sulym <a.sulymm@gmail.com>
2023-03-26 23:23:33 +0100

    Delet

--- new.py
+++ new.py
@@ -1,7 +1,10 @@
+ls = [5, 4, 3, 2, 1]
+
 def find_smaller_digits(ls: list) -> list:
     # write your code here
-    pass
+    result_list = []
+    for i in range(len(ls)):
+        count = 0
+    
 
-
-
-
+print(find_smaller_digits(ls))
