e4c2c49ea5181306a26bac145598adb0915e5866
Andrew Sulym <a.sulymm@gmail.com>
2023-04-19 21:38:05 +0100

    update class

--- First_class.py
+++ First_class.py
@@ -1,14 +1,35 @@
+class Animal:
+    species = "unknown"
+    
+    def __init__(self, name):
+        self.name = name
+        
+    def sound(self):
+        raise NotImplementedError("Subclass must implement abstract method")
+        
+class Dog(Animal):
+    species = "canine"
+    
+    def sound(self):
+        return "Woof!"
+        
+class Cat(Animal):
+    species = "feline"
+    
+    def sound(self):
+        return "Meow!"
+        
+class Cow(Animal):
+    species = "bovine"
+    
+    def sound(self):
+        return "Moo!"
+        
+dog = Dog("Buddy")
+cat = Cat("Smokey")
+cow = Cow("Bessie")
 
-class Point:
-    color = "red"
-    circle = 2
+animals = [dog, cat, cow]
 
-a = Point()
-b = Point()
-a.x = 1
-a.y = 2
-
-b.x = 10
-b.y = 20
-
-print(Point.__dict__)
+for animal in animals:
+    print("{} the {} says '{}'".format(animal.name, animal.species, animal.sound()))
