79f20e6e6e34ced7b108317a5dec21141d0c8109
Andrew Sulym <a.sulymm@gmail.com>
2023-04-26 14:21:40 +0100

    Added new methods, complete task solution

--- Point_Class.py
+++ Point_Class.py
@@ -10,20 +10,42 @@ class Point:
         Point.points.append(self)
 
     
-    def distance_to_origin(self) -> float: # повертає відстань від точки до початку координат
+    def distance_to_origin(self) -> float: 
+        # повертає відстань від точки до початку координат
         return round(math.sqrt(self.x ** 2 + self.y ** 2), 2)
     
     
-    def distance_to_point(self, point: None) -> float: # приймає точку point і повертає відстань від поточної точки до point
+    def distance_to_point(self, point: None) -> float:
+        # приймає точку point і повертає відстань від поточної точки до point
         distance = ((self.x - point.x) ** 2 + (self.y - point.y) ** 2) ** 0.5
         return round(distance, 2)
 
-    def distance_to_x_axis(self: None) -> int: # повертає відстань до осі Х
+    def distance_to_x_axis(self: None) -> int:
+        # повертає відстань до осі Х
         return round(abs(self.y))
     
-    def distance_to_y_axis(self: None) -> int: # повертає відстань до осі Y
+    def distance_to_y_axis(self: None) -> int:
+        # повертає відстань до осі Y
         return round(abs(self.x))
-
+    
+    def find_closest_point(self) -> "Point":
+        # Перевіряємо, чи містить список точок self.points якісь елементи.
+        if not self.points:
+            return None
+        
+        min_distance = None
+        closest_point = None
+        for other_p in self.points:
+            if other_p is not self:
+                distance = self.distance_to_point(other_p)
+                if min_distance is None:
+                    min_distance = distance
+                    closest_point = other_p
+                elif distance < min_distance:
+                    min_distance = distance
+                    closest_point = other_p
+            
+        return closest_point
 
 # створені точки
 point_1 = Point(3, 4)
@@ -36,11 +58,13 @@ point_distanc_origin = point_1.distance_to_origin()
 pointt_distanc_point = point_1.distance_to_point(point_2)
 distanc_to_x = point_1.distance_to_x_axis()
 distanc_to_y = point_1.distance_to_y_axis()
-
+closest_poin = point_1.find_closest_point()
+all_points = Point.points
 
 print(point_distanc_origin)
 print(pointt_distanc_point)
 print(distanc_to_x)
 print(distanc_to_y)
-print()
+print(closest_poin.x, closest_poin.y)
+print(all_points)
 
