5d7ce67953ddc9c1c7d65cf92c5c5728fe68e119
Andrew Sulym <a.sulymm@gmail.com>
2023-05-05 23:13:27 +0100

    Using set, count, to find the maximum occurrence of a number in a list

---
+++ Majority_element.py
@@ -0,0 +1,10 @@
+nums = [0,0,0,34,55]
+
+def majority_element(nums: list) -> int:
+    # write your code here
+    c = len(nums) / 2
+    b = [item for item in set(nums) if nums.count(item) > c]
+    return b[0]
+
+
+print(majority_element(nums))
