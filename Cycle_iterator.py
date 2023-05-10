c4085a62e252ac7353945fc65f92da01ea697167
Andrew Sulym <a.sulymm@gmail.com>
2023-05-10 22:42:00 +0100

    Add mentor solution and doc

--- Cycle_iterator.py
+++ Cycle_iterator.py
@@ -22,6 +22,29 @@ iterator = iter(each)
 print(next(iterator))
 print(next(iterator))
 print(next(iterator))
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
+
+# Итератор бесконечно повторяет элементы списка в том же порядке.
+# Когда мы достигаем конца списка, индекс self.cur_element обнуляется, и итерация начинается сначала.
+
+##### M #####
+
+from __future__ import annotations
+from typing import Iterable, Union
+
+
+class Cycle:
+    def __init__(self, iterable: Iterable) -> None:
+        self.iterable = iterable
+
+    def __iter__(self) -> Cycle:
+        self.current_index = 0
+        return self
+
+    def __next__(self) -> Union[int, str, StopIteration]:
+        if not self.iterable:
+            raise StopIteration
+
+        res = self.iterable[self.current_index]
+        self.current_index = (self.current_index + 1) % len(self.iterable)
+
+        return res
