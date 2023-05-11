f0db644d4455ace3286db02c3a36cc9d95713127
Andrew Sulym <a.sulymm@gmail.com>
2023-05-11 16:25:09 +0100

    Update

---
+++ Prime_number_formula.py
@@ -0,0 +1,14 @@
+num = 11
+
+def simple(num: int) -> int:
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+print(simple(num))
+
+
+
