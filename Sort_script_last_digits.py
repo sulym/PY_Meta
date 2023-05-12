52e0aa953b1e45feb008e94557ea8bf634d89489
Andrew Sulym <a.sulymm@gmail.com>
2023-05-12 21:38:22 +0100

    Script to find the last digits in a string in a file

---
+++ Sort_script_last_digits.py
@@ -0,0 +1,20 @@
+import os # Импортирует модуль os для работы с операционной системой.
+import re # Импортирует модуль re для работы с регулярными выражениями.
+
+
+# Функция get_last_number(), которая принимает строку line,
+# находит последнее число в этой строке с помощью регулярного выражения,
+# и возвращает его в виде целого числа.
+def get_last_number(line: str) -> int:
+    return int(re.findall(r'\d+', line)[-1])
+
+
+with open('ttt', 'r') as file: # Открывает файл ttt для чтения, используя контекстный менеджер with.
+    lines = file.readlines() # Читает все строки из файла ttt и сохраняет их в список lines.
+    
+# Cортирует список lines с помощью функции sorted() и ключа сортировки get_last_number(),
+# Cохраняет отсортированный список строк в sorted_lines.
+sorted_lines = sorted(lines, key=get_last_number)
+
+with open('ttt', 'w') as file: #  Oткрывает файл ttt для записи.
+    file.writelines(sorted_lines) # Записывает все строки из списка sorted_lines в файл ttt.
